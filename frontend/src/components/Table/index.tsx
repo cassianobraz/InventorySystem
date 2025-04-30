6674c8d4450955f83bc37b7c6056020353595ec8
Patrik Malta <maltapatrik3@gmail.com>
2025-04-30 12:08:19 -0400

    adjuste in size of texts

--- frontend/src/components/Table/index.tsx
+++ frontend/src/components/Table/index.tsx
@@ -1,3 +1,7 @@
+import { FaRegEdit } from "react-icons/fa";
+import { MdDeleteOutline } from "react-icons/md";
+
+
 interface Componente {
   codigo: string;
   nome: string;
@@ -13,7 +17,7 @@ export function Table({ dados }: TabelaComponentesProps) {
     <div className="flex justify-center items-center min-h-screen">
       <div className="overflow-x-auto rounded-lg mt-20 w-full max-w-6xl">
         <table className="text-sm text-left w-full">
-          <thead className="text-gray-600 font-bold text-3xl border-b">
+          <thead className="text-gray-600 font-bold text-xl border-b">
             <tr>
               <th className="px-8 py-4">ID</th>
               <th className="px-8 py-4">Produto</th>
@@ -24,18 +28,18 @@ export function Table({ dados }: TabelaComponentesProps) {
           </thead>
           <tbody>
             {dados.map((item, index) => (
-              <tr key={index} className="border-b text-gray-600 text-xl w-full">
+              <tr key={index} className="border-b text-gray-600 text-sm w-full">
                 <td className="px-8 py-5 text-center">{item.codigo}</td>
                 <td className="px-8 py-5 w-full">{item.nome}</td>
-                <td className="px-8 py-5 w-full">{item.saldo}</td>
+                <td className="px-8 py-5 w-full text-center">{item.saldo}</td>
                 <td className="px-8 py-5 w-full">
                   <button className="text-green-600 hover:text-green-800 hover:cursor-pointer w-full">
-                    ✏️
+                    <FaRegEdit className="w-full" />
                   </button>
                 </td>
                 <td className="px-6 py-4">
                   <button className="text-red-500 hover:text-red-700 hover:cursor-pointer w-full">
-                    🗑️
+                    <MdDeleteOutline className="w-full size-6.5" />
                   </button>
                 </td>
               </tr>
